498b0433d018b9ea2d9e7eb605c09e18208c5e3a
Dmitry <36100192+prz13@users.noreply.github.com>
2023-09-13 11:21:54 +0300

    Add_DS.

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -3,9 +3,10 @@ from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(FilterSet):
+    """Фильтр для рецептов."""
     tags = filters.ModelMultipleChoiceFilter(field_name='tags__slug',
-                                             to_field_name='slug',
-                                             queryset=Tag.objects.all())
+                                            to_field_name='slug',
+                                            queryset=Tag.objects.all())
     is_favorited = filters.BooleanFilter(
         method='is_favorited_filter')
     is_in_shopping_cart = filters.BooleanFilter(
