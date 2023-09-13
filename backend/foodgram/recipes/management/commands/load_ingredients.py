636b374981f61a624e876985cc3c3cd0412cc59e
Dmitry <36100192+prz13@users.noreply.github.com>
2023-09-13 20:06:46 +0300

    add_load_ingridients-+.

--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -2,7 +2,7 @@ import csv
 import os
 import logging
 from foodgram import settings
-from tqdm import tqdm  # Импорт tqdm
+from tqdm import tqdm 
 
 from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
@@ -18,17 +18,18 @@ class Command(BaseCommand):
         try:
             with open(path, 'r', encoding='utf-8') as file:
                 reader = csv.reader(file)
-                next(reader)  # Пропустить заголовок
+                next(reader)
 
                 ingredients = [
                     Ingredient(name=row[0], measurement_unit=row[1])
                     for row in tqdm(reader, desc="Loading ingredients", unit=" row")
                 ]
 
-                Ingredient.objects.all().delete()  # Очистить существующие записи
+                Ingredient.objects.all().delete()
                 Ingredient.objects.bulk_create(ingredients)
                 self.stdout.write(self.style.SUCCESS("The ingredients have been loaded successfully."))
         except FileNotFoundError:
             logger.error("File not found: ingredients.csv")
         except csv.Error as e:
             logger.error(f"CSV Error: {e}")
+
