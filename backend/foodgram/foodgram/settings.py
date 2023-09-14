ef1963104535911a6f9ccadfb88a96a31180577d
Dmitry <36100192+prz13@users.noreply.github.com>
2023-09-14 16:21:15 +0300

    Add_80:80.-jobs.

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -118,8 +118,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPaginator',
-    'PAGE_SIZE': 6,
 }
 
 DJOSER = {
