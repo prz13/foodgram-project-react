7a3ea8c67071c74ac431bc25b42e8c9cccd0ed29
Dmitry <36100192+prz13@users.noreply.github.com>
2023-09-15 15:36:48 +0300

    Add Actions

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -4,9 +4,11 @@ from recipes.models import Recipe, Tag
 
 class RecipeFilter(FilterSet):
     """Фильтр для рецептов."""
-    tags = filters.ModelMultipleChoiceFilter(field_name='tags__slug',
-                                            to_field_name='slug',
-                                            queryset=Tag.objects.all())
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
     is_favorited = filters.BooleanFilter(
         method='is_favorited_filter')
     is_in_shopping_cart = filters.BooleanFilter(
