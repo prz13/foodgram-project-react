b2348cb05ee3a37fa476cf4519ec0780167b6394
Dmitry <36100192+prz13@users.noreply.github.com>
2023-09-19 18:48:41 +0300

    Add_flake8.

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.routers import SimpleRouter
 
 from . import views
 
-
 router = SimpleRouter()
 router.register('recipes', views.RecipeViewSet)
 router.register('tags', views.TagViewSet)
