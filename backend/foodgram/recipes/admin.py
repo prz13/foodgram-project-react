37f99bf1922feab065bb2d2f4e589c4d8f6536af
Dmitry <36100192+prz13@users.noreply.github.com>
2023-09-19 18:33:16 +0300

    Add_flake8.

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -11,7 +11,7 @@ class RecipeAdmin(admin.ModelAdmin):
         'pk',
         'name',
         'author',
-        'cooking_time', 
+        'cooking_time',
         'text',
         'image',
         'in_favorites'
