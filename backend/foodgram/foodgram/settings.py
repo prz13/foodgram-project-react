a54884a2dafd0101ca43a4d369bb22aa8db235b3
Dmitry <36100192+prz13@users.noreply.github.com>
2023-09-20 13:51:52 +0300

    Add_.

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = (os.getenv('DEBUG', 'False').lower() == 'true') 
+DEBUG = (os.getenv('DEBUG', 'False').lower() == 'true')
 
 ALLOWED_HOSTS = ['*']
 
